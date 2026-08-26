86d8b9c2f27b0d06a7d4e1a6a146a270be269b6a
The1zero <chandos562@gmail.com>
2026-08-26 15:09:15 -0600

    Day 11

---
+++ TrianguloLPAD.sql
@@ -0,0 +1,19 @@
+SELECT lpad('sql',15,'*');
+
+SELECT lpad ('sql', id, '*')
+FROM platzi.alumnos
+WHERE id < 10;
+
+SELECT lpad('*',id,'*')
+FROM platzi.alumnos
+WHERE id < 10
+ORDER BY carrea_id;
+
+
+SELECT lpad('*',CAST(row_id as int),'*')
+FROM(
+	SELECT ROW_NUMBER() OVER(ORDER BY carrera_id) AS row_id,*
+	FROM platzi.alumnos
+)AS alumnos_with_row_id
+WHERE row_id <= 5
+ORDER BY carrera_id;
